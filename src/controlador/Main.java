b76ea717161bc2aab0dba44fe8c9a166a03c8e9b
Sergi <121488207+SergiMPorto@users.noreply.github.com>
2024-04-27 19:30:14 +0200

    Update Main.java

--- src/controlador/Main.java
+++ src/controlador/Main.java
@@ -1,19 +1,37 @@
 package controlador;
 
-import vista.VentanaLogin;
+import vista.Login;
+import vista.Usuario;
+import vista.Plato;
+import vista.Proveedor;
+
+import java.util.ArrayList;
+
+import vista.Almacen;
 import vista.Home;
-import vista.VentanaAltaProducto;
+import vista.Ingredientes;
+import vista.Pedido;
 
 public class Main {
 
     public static void main(String[] args) {
-        VentanaLogin login = new VentanaLogin();
-        Home home = new Home();
-        VentanaAltaProducto altaProducto = new VentanaAltaProducto();
-        
-        ControladorEventos controlador = new ControladorEventos(login, home, altaProducto);
-        
-        login.iniciarListener(controlador);
-        home.agregarListener(controlador);
-    }
+    	
+   Login login = new Login(); 	
+   Almacen almacen = new Almacen();
+   Pedido pedido = new Pedido();
+   Plato plato = new Plato();
+   Usuario usuario = new Usuario();
+   Home home = new Home();
+   Ingredientes ingredientes = new Ingredientes(new ArrayList<String>());
+   Proveedor proveedor = new Proveedor(null, null, null, null);
+   
+   ControladorEventos controlador = new ControladorEventos(login, home, almacen, pedido, plato, usuario, ingredientes,proveedor);
+   
+   
+   login.inciarListener(controlador);
+   home.agregarListener(controlador);
+   plato.iniciarListerner(controlador);
+   
+       
+}
 }
